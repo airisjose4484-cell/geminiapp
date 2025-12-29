10db32f9fa7e73d73e652a5d6fe238256c5faf01
airisjose4484-cell <airisjose4484@gmail.com>
2025-12-29 12:04:03 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,11 @@
+let result = document.getElementById('result');
+
+function appendNumber(number) {
+    result.value += number;
+}
+
+function appendOperator(operator) {
+    result.value += operator;
+}
+
+function clearResult() {.
